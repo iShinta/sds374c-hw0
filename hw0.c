0c83013c5a4540cdb7609752d0dc4497c5b2ca31
iShinta <ho_minh_tri@yahoo.fr>
2017-02-08 00:21:30 -0600

    Test randlight3

--- hw0.c
+++ hw0.c
@@ -18,7 +18,7 @@ void initialize(double *x, int n){
 
       nest = 10;
       while(nest--){
-        random = rand()%1 + 1;
+        random = (double)((rand()%10000)/10000);
         printf("%d\n", random);
       }
 
